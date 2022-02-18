7f0a773720db12f346409daf44ba4c6936ed5a25
charlesMayConequip <charles.may@conequip.com>
2022-02-18 16:09:16 -0500

    items added to cart now go into state and then are stored in local storage, then when you go to the cart screen they are pulled back into the state

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,14 +1,26 @@
 import React, {useEffect} from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams, useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import { Message } from '../components/Message'
 import { addToCart } from '../actions/cartActions'
 
 function CartScreen({match, location, history}) {
-    const productId = match.params.id
-    const qty = location.search
-    console.log('qty ', qty)
+    const[searchParams, setSearchParams] = useSearchParams();
+    const qty = Number(searchParams.get('qty'))
+    const { id } = useParams();
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const {cartItems} = cart
+    console.log('cart items: ', cartItems)
+
+    useEffect(() => {
+        if (id) {
+            dispatch(addToCart(id, qty))
+        }
+    }, [dispatch, id, qty])
 
   return (
     <div>Cart</div>
