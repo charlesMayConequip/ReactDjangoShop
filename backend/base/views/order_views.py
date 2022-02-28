f52ff8cbd95a23628851913aff0e96e88999b204
charlesMayConequip <charles.may@conequip.com>
2022-02-28 13:45:19 -0500

    made page for customers order to be viewed at

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -63,3 +63,18 @@ def addOrderItems(request):
         serializer = OrderSerializer(order, many=False)
         return Response(serializer.data)
     
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrderById(request, pk):
+
+    user = request.user
+
+    try:
+        order = Order.objects.get(_id=pk)
+        if user.is_staff or order.user == user:
+            serializer = OrderSerializer(order, many=False)
+            return Response(serializer.data)
+        else:
+            Response({'detail': 'Not authorized to view this oder'}, status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response({'detail': 'Order does not exist.'}, status=status.HTTP_400_BAD_REQUEST)
