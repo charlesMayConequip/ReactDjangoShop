e559b8bb2689dce95e0f39c47700c6db2f140c95
charlesMayConequip <charles.may@conequip.com>
2022-03-08 08:52:14 -0500

    final push before making react static for hosting

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -214,6 +214,7 @@ function ProductScreen({ match }) {
                       disabled={loadingProductReview}
                       type='submit'
                       variant='primary'
+                      className="mt-3"
                     >
                       Submit
                     </Button>
